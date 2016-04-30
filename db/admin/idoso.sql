0bf61559e7410a82c9c89a34e8e54f5727a87541
gabydias@gmail.com <gabydias@gmail.com>
2016-04-30 14:48:52 -0300

    criacao tabela idoso

---
+++ db/admin/idoso.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `idoso` (
+  `id` int(11) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `nome_contato` varchar(50) NOT NULL,
+  `grau_parentesco` varchar(50) NOT NULL,
+  `telefone_contato` varchar(10) DEFAULT NULL,
+  `celular_contato` varchar(11) NOT NULL,
+  `necessidades_especiais` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
